fe0a2e182b1627c12823c50ef64334836d70381d
AuraAsteraceae <alantsang.work@gmail.com>
2018-02-26 18:14:55 -0800

    Fixed sessions i think pushing back to server

--- routes/session.js
+++ routes/session.js
@@ -15,64 +15,61 @@ class Session
 
 function startSession(id, callback)
 {
-	//console.log("StartSession Start");
-	//if (!findSession(id))
-	//{
-		let sessionID = crypto.randomBytes(tokenSize).toString("hex");
-		let token = crypto.randomBytes(tokenSize).toString("hex");
-		//Implement this properly later.
-		//let date = new Date();
-		let time = 0;
-		let session = new Session(sessionID, token, time);
-		//activeSessions[id] = session;
-		client.hmset(id, {activeSession: session}, function(err) 
-      {console.log("Session hmset error");}
-    );
-		console.log("StartSession Callback");
-		process.nextTick( () => { callback(session); } );
-	//}
-	//else
-	//{
-		//Session already exists but make a new one cuz dumb.
-		
-	//}
-	//console.log("StartSession End");
+  //console.log("StartSession Start");
+  //if (!findSession(id))
+  //{
+  	let sessionID = crypto.randomBytes(tokenSize).toString("hex");
+  	let token = crypto.randomBytes(tokenSize).toString("hex");
+  	//Implement this properly later.
+  	//let date = new Date();
+  	let time = 0;
+  	let session = new Session(sessionID, token, time);
+  	//activeSessions[id] = session;
+  	client.hmset(id, session, (err, obj) =>
+    {
+      if (err) console.log("error in startSession: " + err);
+      console.log("StartSession Callback");
+      process.nextTick( () => { callback(session); } );
+    });
+  //}
+  //else
+  //{
+  	//Session already exists but make a new one cuz dumb.
+  	
+  //}
+  //console.log("StartSession End");
 }
 
 function endSession(id, callback)
 {
-	//console.log("EndSession Start");
-	let session = findSession(id);
-
-	if (session)
-	{
-		//Get time
-		//let currDate = Date();
-		//let currTime = currDate.getSeconds();
-		let duration = 0;//currTime - session.time;//GetCurrTime
-		
-		delete session;
-		client.hdel(id, "activeSession",  function(err) {} );
-		console.log("EndSession Callback");
-		process.nextTick( () => {callback(duration); } );
-	}
-	//console.log("EndSession End");
+  //console.log("EndSession Start");
+  findSession(id, (err, session) =>
+  {
+    if (err) console.log("error in endSession: " + err);
+    if (session)
+    {
+      //Get time
+      //let currDate = Date();
+      //let currTime = currDate.getSeconds();
+      let duration = 0;//currTime - session.time;//GetCurrTime
+      //TODO: Delete but we don't need to atm.
+      //delete session;
+      //client.hdel(id, function(err) {} );
+      console.log("EndSession Callback");
+      process.nextTick( () => {callback(duration); } );
+    }
+  });
+  //console.log("EndSession End");
 }
 
-function findSession(id)
+function findSession(id, callback)
 {
-	//console.log("FindSession Start");
-	//let found = activeSessions[id];
-	client.hgetall(id, function(err, object)
+  //console.log("FindSession Start");
+  //let found = activeSessions[id];
+  client.hgetall(id, (err, object) =>
   {
-    if (object)
-    {
-      return object;
-    }
-    else
-    {
-      return null;
-    }
+    if (err) console.log("error in findSession: " + err);
+    callback(object);
   });
 }
 
